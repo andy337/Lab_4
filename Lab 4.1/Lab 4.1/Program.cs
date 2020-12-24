b79f4f2c272f3abb4e521fa21dcde4a8da922adb
andy337 <52741688+andy337@users.noreply.github.com>
2020-12-24 09:56:33 +0200

    Add files via upload

---
+++ Lab 4.1/Lab 4.1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+using System.Text.RegularExpressions;
+
+namespace Lab_4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex regexExtForImage = new Regex(@"((bmp)|(gif)|(tiff)|(jpeg)|(png))$", RegexOptions.IgnoreCase);
+            string path = @"C:\Users\Admin\source\repos\Lab_4.1\Lab_4.1\Test";
+            string[] files = Directory.GetFiles(path);
+            foreach (string file in files)
+            {
+                if (regexExtForImage.IsMatch(Path.GetExtension(file)))
+                {
+                    Image img = Image.FromFile(file);
+                    img.RotateFlip(RotateFlipType.Rotate180FlipY);
+                    img.Save(regexExtForImage.Replace(file, " ") + "-mirrored.gif");
+                }
+            }
+        }
+    }
+}
